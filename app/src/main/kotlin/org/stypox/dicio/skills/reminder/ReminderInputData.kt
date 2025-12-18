edae993281919f7c4495fdd5cab3699a0941c4aa
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-12-17 23:31:42 -0500

    Create ReminderInputData.kt

---
+++ app/src/main/kotlin/org/stypox/dicio/skills/reminder/ReminderInputData.kt
@@ -0,0 +1,10 @@
+package org.stypox.dicio.skills.reminder
+
+import org.dicio.skill.SkillData
+
+data class ReminderInputData(
+    val sentenceId: String,
+    val text: String?,
+    val time: String?,
+    val index: String?
+) : SkillData
