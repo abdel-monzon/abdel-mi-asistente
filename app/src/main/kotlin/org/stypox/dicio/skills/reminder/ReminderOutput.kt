3630abb4295b1ebc2d35563665042d0b10b9c06e
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-12-17 23:33:37 -0500

    Create ReminderOutput.kt

---
+++ app/src/main/kotlin/org/stypox/dicio/skills/reminder/ReminderOutput.kt
@@ -0,0 +1,15 @@
+package org.stypox.dicio.skills.reminder
+
+import org.dicio.skill.SkillContext
+import org.dicio.skill.SkillOutput
+import org.stypox.dicio.R
+
+class ReminderOutput(
+    private val messageRes: Int,
+    private vararg val args: Any
+) : SkillOutput {
+
+    override suspend fun generate(ctx: SkillContext): String {
+        return ctx.android.getString(messageRes, *args)
+    }
+}
