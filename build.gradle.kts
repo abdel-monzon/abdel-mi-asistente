a3073a44d8390aafaf6d367871dadab668c910ff
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-12-19 15:28:32 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // 📄 Archivo: build.gradle.kts (en la RAIZ del proyecto)
 plugins {
-    // Usa la forma completa con 'id' y especifica la versión
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    // Agrega esta línea para resolver el conflicto de parcelize
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.0.0" apply false
 }
