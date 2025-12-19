1f7b5c9950093aefa3f3ff64901f7e1c095550c1
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-12-19 00:59:54 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // 📄 Archivo: build.gradle.kts (en la RAIZ del proyecto)
 plugins {
-    // 1. Declara el plugin de Kotlin aquí, con 'apply false'
-    kotlin("android") version "2.0.0" apply false
-    // 2. NO declares otros plugins (Hilt, etc.) aquí.
+    // Solo declara el plugin de Kotlin aquí, SIN la versión.
+    // La versión será tomada automáticamente desde 'gradle/libs.versions.toml'
+    kotlin("android") apply false
 }
