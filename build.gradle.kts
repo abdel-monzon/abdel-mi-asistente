7566836cb4a863233c49af3517bf83736c17e946
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-12-19 00:43:09 -0500

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// 📄 Archivo: build.gradle.kts (en la RAIZ del proyecto)
+plugins {
+    // 1. Declara el plugin de Kotlin aquí, con 'apply false'
+    kotlin("android") version "2.0.0" apply false
+    // 2. NO declares otros plugins (Hilt, etc.) aquí.
+}
