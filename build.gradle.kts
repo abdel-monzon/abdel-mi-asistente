6bf30e93ad3be339818f9febc9b8674f56111374
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-12-19 23:01:31 -0500

    Delete build.gradle.kts
    Eliminarlo completo no lo necesito en el proyecto

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// 📄 Archivo: build.gradle.kts (en la RAIZ del proyecto)
-plugins {
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    // Agrega esta línea para resolver el conflicto de parcelize
-    id("org.jetbrains.kotlin.plugin.parcelize") version "2.0.0" apply false
-}
