fdb38f803d396e6e649260f0494c66bef368e9b6
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-12-19 22:32:11 -0500

    Update build.gradle.kts

--- skill/build.gradle.kts
+++ skill/build.gradle.kts
@@ -1,9 +1,9 @@
+
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.com.android.library)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
-    alias(libs.plugins.org.jetbrains.kotlin.plugin.compose)
+    // SOLO library y maven-publish, SIN plugins de Kotlin
     id("maven-publish")
 }
 
@@ -32,14 +32,9 @@ tasks.withType<Test>().configureEach {
 }
 
 dependencies {
-    // dicio-numbers is needed to bring ParserFormatter into the classpath
     implementation(libs.dicio.numbers)
-
-    // Compose (check out https://developer.android.com/jetpack/compose/bom/bom-mapping)
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
-
-    // Testing
     testImplementation(libs.kotest.runner.junit5)
     testImplementation(libs.kotest.assertions.core)
     testImplementation(libs.kotest.property)
