0a4eda7c9281c7327f6d84d8320c69b04f8357bd
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-12-27 23:07:17 -0500

    Update ErrorInfo.kt

--- app/src/main/kotlin/org/stypox/dicio/error/ErrorInfo.kt
+++ app/src/main/kotlin/org/stypox/dicio/error/ErrorInfo.kt
@@ -2,7 +2,8 @@ package org.stypox.dicio.error
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import org.stypox.dicio.util.ExceptionUtils
+import java.io.PrintWriter
+import java.io.StringWriter
 
 /**
  * @implNote Taken with some modifications from NewPipe, file error/ErrorInfo.kt
@@ -10,8 +11,16 @@ import org.stypox.dicio.util.ExceptionUtils
 @Parcelize
 class ErrorInfo(val stackTrace: String, val userAction: UserAction) : Parcelable {
     constructor(throwable: Throwable?, userAction: UserAction) : this(
-        if (throwable == null) "" else ExceptionUtils.getStackTraceString(throwable),
+        if (throwable == null) "" else getStackTraceString(throwable),
         userAction
     )
+    
+    companion object {
+        private fun getStackTraceString(throwable: Throwable): String {
+            val sw = StringWriter()
+            val pw = PrintWriter(sw)
+            throwable.printStackTrace(pw)
+            return sw.toString()
+        }
+    }
 }
-
